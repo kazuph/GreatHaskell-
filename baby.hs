abee736e68f78196f7be9d3ba73ab2d5f2cd93fc
Kazuhiro Homma <kazu.homma@gmail.com>
2013-01-19 19:55:38 +0900

    ちょっと試行錯誤中

--- baby.hs
+++ baby.hs
@@ -1,4 +1,10 @@
-doubleMe x = x + x
-doubleUs x y = doubleMe x + doubleMe y
-doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
-main = putStrLn "I am baby."
+main = do
+        let doubleMe x = x + x
+        let doubleUs x y = doubleMe x + doubleMe y
+        let doubleSmallNumber x = if x > 100
+                                      then x
+                                      else x * 2
+        let doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+        putStrLn "I am baby."
+        doubleUs 4 9
+
