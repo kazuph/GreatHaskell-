608cc9168ea40f258c49927cff51ae18bd16b503
Kazuhiro Homma <kazu.homma@gmail.com>
2013-01-30 09:16:13 +0900

    section 3 途中

--- match.hs
+++ match.hs
@@ -14,6 +14,36 @@ factorial :: Int -> Int
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
 
-factorial' :: Integer -> Integer
-factorial' 0 = 1
-factorial' n = n * factorial (n - 1)
+charName :: Char -> String
+charName 'a' = "Albert"
+charName 'b' = "Broseph"
+charName 'c' = "Cecil"
+
+addVectors :: (Double, Double) -> (Double, Double) -> (Double, Double)
+addVectors a b = (fst a + fst b, snd a + snd b)
+
+addVectors' :: (Double, Double) -> (Double, Double) -> (Double, Double)
+addVectors' (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (a, _, _) = a
+second :: (a, b, c) -> b
+second (_, b, _) = b
+third :: (a, b, c) -> c
+third (_, _, c) = c
+
+xs = [(1, 3), (4, 3), (2, 4), (5, 3), (5, 6), (3, 1)]
+
+head' :: [a] -> a
+head' [] = error "Can't call head on an empty list, dummuy!"
+head' ( x:_ ) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list empty"
+tell (x:[]) = "Teh list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "The list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+
+firstLetter :: String -> String
+firstLetter "" = "Empty stirng, whoops!"
+firstLetter all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
